1f460a7d2a92748931c8971a7340b4372f7e629b
Yousif Mohamed <yousif.infinix@gmail.com>
2023-12-03 20:42:11 +0200

    free and protect

--- Processor.vhd
+++ Processor.vhd
@@ -30,6 +30,20 @@ component InstructionMemory IS
     );
 END component;
 
+component RegFile IS
+  PORT (
+    reg_write : IN STD_LOGIC;
+    write_data : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    rst : IN STD_LOGIC;
+    read_reg1 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    read_reg2 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    read_data1 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    read_data2 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    write_reg : IN STD_LOGIC_VECTOR(2 DOWNTO 0)
+  );
+END component;
+
 component ALU is
 port(
 	R1, R2: in std_logic_vector(31 downto 0);
--- dataMemory.vhd
+++ dataMemory.vhd
@@ -4,9 +4,10 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 ENTITY DataMemory IS
     PORT (
-        Clk : IN STD_LOGIC;
         MemWrite : IN STD_LOGIC;
         MemRead : IN STD_LOGIC;
+	protectSig: in std_logic;
+	freeSig: in std_logic;
         DataIn : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         DataOut : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         Addr : IN STD_LOGIC_VECTOR(19 DOWNTO 0)
@@ -15,18 +16,28 @@ END DataMemory;
 
 ARCHITECTURE Behavioral OF DataMemory IS
     TYPE MemoryArray IS ARRAY (0 TO 2047) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
-    SIGNAL Memo : MemoryArray;
+    SIGNAL Memo : MemoryArray := (others => (others => '0'));
+    TYPE ProtectArray IS ARRAY (0 TO 2047) OF STD_LOGIC;
+    SIGNAL protect : ProtectArray;
 
 BEGIN
-    PROCESS (Clk)
+    PROCESS (MemWrite, MemRead, dataIn, addr)
     BEGIN
-        IF rising_edge(Clk) THEN
-            IF MemWrite = '1' THEN
+            IF MemWrite = '1' and protect(to_integer(unsigned(Addr(10 DOWNTO 0)))) = '0' THEN
                 Memo(to_integer(unsigned(Addr(10 DOWNTO 0)))) <= DataIn(31 DOWNTO 16);
                 Memo(to_integer(unsigned(Addr(10 DOWNTO 0))) + 1) <= DataIn(15 DOWNTO 0);
             END IF;
-        END IF;
     END PROCESS;
+
+    PROCESS (protectSig, freeSig)
+    BEGIN
+            IF protectSig = '1' THEN
+                protect(to_integer(unsigned(Addr(10 DOWNTO 0)))) <= '1';
+	    elsif freeSig = '1' then
+		protect(to_integer(unsigned(Addr(10 DOWNTO 0)))) <= '0';
+            END IF;
+    END PROCESS;
+
 
     DataOut(31 DOWNTO 16) <= (OTHERS => '0') WHEN MemRead = '0' ELSE
     Memo(to_integer(unsigned(Addr(10 DOWNTO 0))));
