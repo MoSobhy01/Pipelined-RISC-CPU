039e082562bb72fe8006f0883f1709ea05883501
John Doe <myehia162@gmail.com>
2023-12-04 13:59:48 +0200

    instruction memory tested

--- instructionMemory.vhd
+++ instructionMemory.vhd
@@ -15,29 +15,32 @@ ARCHITECTURE Behavioral OF InstructionMemory IS
     TYPE MemoryArray IS ARRAY (0 TO 2047) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL instrMem : MemoryArray;
 
-    FILE memfile : text;
-
 BEGIN
-    PROCESS
+    PROCESS (pc, address)
         VARIABLE line_buffer : LINE;
-        VARIABLE data : INTEGER;
+        VARIABLE data : STD_LOGIC_VECTOR(15 DOWNTO 0);
+        FILE memfile : text;
+        VARIABLE i : INTEGER := 0;
     BEGIN
         FOR i IN 0 TO 2047 LOOP
             instrMem(i) <= (OTHERS => '0');
         END LOOP;
 
-        file_open(memfile, "instructionMem.txt", read_mode);
-        FOR i IN 0 TO 2047 LOOP
+        file_open(memfile, "E:\ModelSim\project\instructionMem.txt", read_mode);
+        WHILE((i < 2048) AND (NOT endfile(memfile))) LOOP
             readline(memfile, line_buffer);
             read(line_buffer, data);
-            instrMem(i) <= STD_LOGIC_VECTOR(to_unsigned(data, instrMem(i)'length));
+
+            REPORT "line inst : ---------------- " & to_hstring(data);
+                instrMem(i) <= data;
+            i := i + 1;
         END LOOP;
         file_close(memfile);
-
         address <= pc(12 DOWNTO 0);
+
         instr <= instrMem(to_integer(unsigned(address)));
 
-        WAIT;
+        --  WAIT;
     END PROCESS;
 
 END Behavioral;
