707a1a712d1204eae844613029cdf50e51c8d2a3
Yousif Mohamed <yousif.infinix@gmail.com>
2023-12-19 20:47:20 +0200

    swap detection

--- Processor.vhd
+++ Processor.vhd
@@ -14,35 +14,6 @@ ENTITY Processor IS
 END Processor;
 ARCHITECTURE ArchProcessor OF Processor IS
 
-    --global helper variable
-    CONSTANT signal_count : INTEGER := 16; --Alu_op takes 4 slots
-
-    --signal bus
-    SIGNAL ImmSrc, Branch, BranchIf0, MemRead, MemWrite, SpOp, Protect, Free, MemWb, RegWrite, PortWrite, PortWB : STD_LOGIC;
-    SIGNAL AluOp : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL signal_vector : STD_LOGIC_VECTOR(signal_count - 1 DOWNTO 0);
-    SIGNAL instruction : STD_LOGIC_VECTOR(15 DOWNTO 0);
-    SIGNAL read_reg1, read_reg2, write_reg : STD_LOGIC_VECTOR(2 DOWNTO 0);
-    SIGNAL reg_read_data1, reg_read_data2, reg_write_data : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL alu_in1, alu_in2, alu_out, data_mem_in, data_mem_out : STD_LOGIC_VECTOR (31 DOWNTO 0);
-    SIGNAL data_mem_address : STD_LOGIC_VECTOR (19 DOWNTO 0);
-    SIGNAL immidiate_value : STD_LOGIC_VECTOR (15 DOWNTO 0);
-    SIGNAL immidiate_alu_in : STD_LOGIC_VECTOR (31 DOWNTO 0);
-    SIGNAL immidiate_flag : STD_LOGIC := '0';
-    SIGNAL WB_selector : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    SIGNAL zero_vector16 : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL one_vector16 : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '1');
-    SIGNAL zero_vector32 : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL one_vector32 : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '1');
-    --registers
-    SIGNAL pc, sp : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL CCR : STD_LOGIC_VECTOR(2 DOWNTO 0);
-    SIGNAL CCR_next : STD_LOGIC_VECTOR(2 DOWNTO 0);
-    SIGNAL if_id_reg : STD_LOGIC_VECTOR(47 DOWNTO 0);
-    SIGNAL id_ex_reg : STD_LOGIC_VECTOR(130 DOWNTO 0);
-    SIGNAL ex_mem_reg : STD_LOGIC_VECTOR(107 DOWNTO 0);
-    SIGNAL mem_wb_reg : STD_LOGIC_VECTOR(69 DOWNTO 0);
-
     COMPONENT InstructionMemory IS
         PORT (
             rst : IN STD_LOGIC;
@@ -130,6 +101,42 @@ ARCHITECTURE ArchProcessor OF Processor IS
             F_mux : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0));
     END COMPONENT;
 
+     -------------global helper variable-------------
+     CONSTANT signal_count : INTEGER := 16; --Alu_op takes 4 slots
+
+     -----------------signal bus--------------------
+     SIGNAL ImmSrc, Branch, BranchIf0, MemRead, MemWrite, SpOp, Protect, Free, MemWb, RegWrite, PortWrite, PortWB : STD_LOGIC;
+     SIGNAL AluOp : STD_LOGIC_VECTOR(3 DOWNTO 0);
+     SIGNAL signal_vector : STD_LOGIC_VECTOR(signal_count - 1 DOWNTO 0);
+     SIGNAL instruction : STD_LOGIC_VECTOR(15 DOWNTO 0);
+     SIGNAL read_reg1, read_reg2, write_reg : STD_LOGIC_VECTOR(2 DOWNTO 0);
+     SIGNAL reg_read_data1, reg_read_data2, reg_write_data : STD_LOGIC_VECTOR(31 DOWNTO 0);
+     SIGNAL alu_in1, alu_in2, alu_out, data_mem_in, data_mem_out : STD_LOGIC_VECTOR (31 DOWNTO 0);
+     SIGNAL data_mem_address : STD_LOGIC_VECTOR (19 DOWNTO 0);
+     SIGNAL immidiate_value : STD_LOGIC_VECTOR (15 DOWNTO 0);
+     SIGNAL immidiate_alu_in : STD_LOGIC_VECTOR (31 DOWNTO 0);
+     SIGNAL immidiate_flag : STD_LOGIC := '0';
+     SIGNAL WB_selector : STD_LOGIC_VECTOR(1 DOWNTO 0);
+     SIGNAL zero_vector16 : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
+     SIGNAL one_vector16 : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '1');
+     SIGNAL zero_vector32 : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
+     SIGNAL one_vector32 : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '1');
+     --swap
+     signal swapStall: STD_LOGIC;
+     signal swap_ins1: std_logic_vector(15 downto 0);
+     signal swap_ins2: std_logic_vector(15 downto 0);
+
+
+     -----------------registers--------------------
+     SIGNAL pc, sp : STD_LOGIC_VECTOR(31 DOWNTO 0);
+     SIGNAL CCR : STD_LOGIC_VECTOR(2 DOWNTO 0);
+     SIGNAL CCR_next : STD_LOGIC_VECTOR(2 DOWNTO 0);
+     SIGNAL if_id_reg : STD_LOGIC_VECTOR(47 DOWNTO 0);
+     SIGNAL id_ex_reg : STD_LOGIC_VECTOR(130 DOWNTO 0);
+     SIGNAL ex_mem_reg : STD_LOGIC_VECTOR(107 DOWNTO 0);
+     SIGNAL mem_wb_reg : STD_LOGIC_VECTOR(69 DOWNTO 0);
+
+
 BEGIN
     --components
     u0 : InstructionMemory PORT MAP(rst, pc, instruction);
@@ -140,6 +147,10 @@ BEGIN
     u5 : mux4 PORT MAP(mem_wb_reg(34 DOWNTO 3), mem_wb_reg(66 DOWNTO 35), in_port, in_port, WB_selector, reg_write_data); --Write back mux
     u6 : mux2 PORT MAP(id_ex_reg(34 DOWNTO 3), immidiate_alu_in, id_ex_reg(71), alu_in2);
     u7 : mux2 GENERIC MAP(16) PORT MAP(zero_vector16, instruction, immidiate_flag, immidiate_value);
+    
+    u8 : swapDetection PORT MAP(instruction, swapStall, swap_ins);
+    u9 : mux2 PORT MAP(instruction, swap_ins, swapStall, instruction);
+
 
     --connections
 
@@ -171,20 +182,22 @@ BEGIN
             id_ex_reg <= (OTHERS => '0');
             ex_mem_reg <= (OTHERS => '0');
             mem_wb_reg <= (OTHERS => '0');
+        
+        ELSIF falling_edge(clk) THEN
 
         ELSIF rising_edge(clk) THEN
             pc <= STD_LOGIC_VECTOR(unsigned(pc) + 1);
             CCR <= CCR_next;
             IF immidiate_flag = '0' THEN
                 immidiate_flag <= instruction(0);
-                if_id_reg(47 DOWNTO 0) <= pc & instruction;
+                if_id_reg(48 DOWNTO 0) <= swapStall & pc & instruction;
             ELSE
                 immidiate_flag <= '0';
-                if_id_reg(47 DOWNTO 0) <= (OTHERS => '0');
+                if_id_reg(48 DOWNTO 0) <= (OTHERS => '0');
             END IF;
-            id_ex_reg(130 DOWNTO 0) <= immidiate_value & if_id_reg(47 DOWNTO 16) & signal_vector & reg_read_data1 & reg_read_data2 & if_id_reg(9 DOWNTO 7);
-            ex_mem_reg(107 DOWNTO 0) <= id_ex_reg(112 DOWNTO 81) & id_ex_reg(82 DOWNTO 74) & alu_out & alu_in2 & id_ex_reg(2 DOWNTO 0);
-            mem_wb_reg(69 DOWNTO 0) <= ex_mem_reg(75) & ex_mem_reg(73 DOWNTO 72) & data_mem_out & data_mem_in & ex_mem_reg(2 DOWNTO 0);
+            id_ex_reg(131 DOWNTO 0) <= if_id_reg(48) & immidiate_value & if_id_reg(47 DOWNTO 16) & signal_vector & reg_read_data1 & reg_read_data2 & if_id_reg(9 DOWNTO 7);
+            ex_mem_reg(106 DOWNTO 0) <= id_ex_reg(114 DOWNTO 83) & id_ex_reg(81 DOWNTO 74) & alu_out & alu_in2 & id_ex_reg(2 DOWNTO 0);
+            mem_wb_reg(68 DOWNTO 0) <= ex_mem_reg(73 DOWNTO 72) & data_mem_out & data_mem_in & ex_mem_reg(2 DOWNTO 0);
         END IF;
     END PROCESS;
 END ArchProcessor;
