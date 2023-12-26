58d777264d9057333a0f007f1da102e3c9be7f41
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-12-26 23:43:00 +0200

    updating SP

--- SP.vhd
+++ SP.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY SP_Circuit IS
   PORT (
@@ -16,20 +17,21 @@ BEGIN
   PROCESS (clk, reset) IS
   BEGIN
     IF (reset = '1') THEN
-      SP_Reg <= (OTHERS => '0);
+      SP_Reg <= (OTHERS => '0');
     ELSIF (rising_edge(clk)) THEN
       IF (MemWrite = '1') THEN
         SP_out_reg <= STD_LOGIC_VECTOR(unsigned(SP_Reg) + 1);
       ELSE
         SP_out_reg <= SP_Reg;
-      ELSIF falling_edge(clk) AND enable THEN
-        IF (MemWrite = '1') THEN
-          SP_Reg <= STD_LOGIC_VECTOR(unsigned(SP_Reg) + 1);
-        ELSE
-          SP_Reg <= STD_LOGIC_VECTOR(unsigned(SP_Reg) - 1);
-        END IF;
       END IF;
-    END PROCESS;
+    ELSIF falling_edge(clk) AND enable = '1' THEN
+      IF (MemWrite = '1') THEN
+        SP_Reg <= STD_LOGIC_VECTOR(unsigned(SP_Reg) + 1);
+      ELSE
+        SP_Reg <= STD_LOGIC_VECTOR(unsigned(SP_Reg) - 1);
+      END IF;
+    END IF;
+  END PROCESS;
 
-    SP_Out <= SP_out_reg;
-  END ARCHITECTURE Behavioral;
+  SP_Out <= SP_out_reg;
+END ARCHITECTURE Behavioral;
